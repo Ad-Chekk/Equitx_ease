3bb8fc9a4a3c99cd10d06e86d3eab1d9ddf9ddd5
Aditya Chekkala <123799525+Ad-Chekk@users.noreply.github.com>
2025-01-28 15:24:12 +0530

    yolo

--- pages/2_Single_equity.py
+++ pages/2_Single_equity.py
@@ -95,8 +95,10 @@ if stock_input and start_date and selected_option:
   # paper_bgcolor='rgba(0, 0, 0, 0)',  # Transparent background
   # plot_bgcolor='rgba(0, 0, 0, 0)',  # Transparent plot area
   # )
-  line.update_layout( height=350, width=800)
-  st.plotly_chart(line)
+  line.update_layout(height=350)
+  st.plotly_chart(line, use_container_width=True)
+  # line.update_layout( height=350, width=800)
+  # st.plotly_chart(line)
 
 
 
